fd25dd828b3c19075fb570d344e2f8a313265114
supertamzer <60706571+supertamzer@users.noreply.github.com>
2020-06-09 16:15:49 +0200

    input + grille

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,46 @@
+let input, button;
+let gridNumber;
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+    createCanvas(windowWidth, windowHeight);
+
+    input = createInput();
+    input.position(1250, 25);
+
+    button = createButton('submit');
+    button.position(input.x + input.width, 25);
+    button.mousePressed(greet);
+
+
+    textAlign(CENTER);
+
 }
 
-function draw() { 
+function draw() {
+    for (let i = 0; i < height; i += height / 8) {
+        for (let j = 0; j < width; j += 100) {
+            line(0, i, width, i);
+            line(j, 0, j, height);
+            text(i + "/" + j, i, j);
+        }
+    }
+
+    ellipse() =
+
+
+
+        console.log(gridNumber);
 
 }
+
+function greet() {
+    const name = input.value();
+    input.value('');
+    fill(0);
+    text(name, width / 2, height / 2);
+}
+
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+    resizeCanvas(windowWidth, windowHeight);
 }
